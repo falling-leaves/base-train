eac0a1b6f63bfc516ec0f5f394c66e193c43a436
Bon Leif Amalla <bonleifamalla@gmail.com>
2020-03-11 15:25:41 +0800

    Use external JS to store courses.
    Delete this later if solution to read hosted JSON exists.

---
+++ js/courses.js
@@ -0,0 +1,26 @@
+var courses = [
+    {
+        "title": "Single digit addition/subtraction",
+        "flav": "Addition of single digit numbers"
+    },
+    {
+        "title": "General addition/subtraction",
+        "flav": "Addition and subtraction for natural numbers"
+    },
+    {
+        "title": "Logical statements",
+        "flav": "Simplification of simple logical statements"
+    },
+    {
+        "title": "Binary addition/subtraction",
+        "flav": "Addition of numbers expressed in binary"
+    },
+    {
+        "title": "Binary multiplication",
+        "flav": "Multiplication of numbers expressed in binary"
+    },
+    {
+        "title": "Boolean algebra in binary",
+        "flav": "Perform logical operations on numbers in binary"
+    }
+]
